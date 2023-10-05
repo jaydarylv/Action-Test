c12dac094a4d12fd8343878eb0a14126c927a589
jaydarylv <106700137+jaydarylv@users.noreply.github.com>
2023-10-05 23:48:34 +0800

    Create test.py
    sample python file without issue

---
+++ .github/workflows/test.py
@@ -0,0 +1,14 @@
+def add_numbers(x, y):
+    return x + y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def greet(name):
+    return f"Hello, {name}!"
+
+result = add_numbers(3, 4)
+print(result)
+
+message = greet("Alice")
+print(message)
